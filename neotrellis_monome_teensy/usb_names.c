ce309ab770935dd1bc24fdc09663db55a6bcbe6b
Ben Alkov <ben.alkov@gmail.com>
2020-02-08 15:06:21 -0500

    Add paletted code from oldmanfury

--- neotrellis_monome_teensy/usb_names.c
+++ neotrellis_monome_teensy/usb_names.c
@@ -2,13 +2,13 @@
 
 // USED BY TEENSY TO DEFINE USB PRODUCT/MANUFACTURER/SERIAL INFO
 
-#define PRODUCT_NAME      {'m','o','n','o','m','e'}
-#define PRODUCT_NAME_LEN   6
+#define PRODUCT_NAME      {'n','e', 'o', 'n','o','m','e'}
+#define PRODUCT_NAME_LEN  4
 #define MANUFACTURER_NAME  {'m','o','n','o','m','e'}
 #define MANUFACTURER_NAME_LEN 6
 
-// Change this to something different if you like - but you MUST keep the "m" as the first character.
-#define SERIAL_NUM  {'m','4','6','7','6','0','5','5'}
+// Change this to something different if you like - but keep the "m" as the first character.
+#define SERIAL_NUM  {'m','9','0','6','7','8','4','5'}
 #define SERIAL_NUM_LEN 8
 
 struct usb_string_descriptor_struct usb_string_product_name = {
